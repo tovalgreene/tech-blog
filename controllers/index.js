697eac468ad44bb64491ee52a96a64f468015f94
Tova Greene <tovalgreene@gmail.com>
2023-11-13 11:55:32 -0500

    wrote index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboardController.js');
+const homeRoutes = require('./homeController.js');
 
-const homeRoutes = require('./homeController');
-const postRoutes = require('./postController');
-const userRoutes = require('./userController');
-
+// Route setup
 router.use('/', homeRoutes);
-router.use('/posts', postRoutes); 
-router.use('/users', userRoutes); 
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
+
+// 404 Not Found handler
+router.use((req, res) => {
+    res.status(404).send('404 Not Found');
+});
 
 module.exports = router;
