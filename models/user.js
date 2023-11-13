c84e39b9dc14475f9cb653fe5e08d549aa5aa2f6
Tova Greene <tovalgreene@gmail.com>
2023-11-13 12:05:39 -0500

    edited user.js

--- models/user.js
+++ models/user.js
@@ -1,47 +1,63 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const post = require('./post');
-const comment = require('./comment');
+const bcrypt = require('bcrypt');
 
-class user extends Model { }
+// Create the User model with methods and hooks
+class User extends Model {
+    // Method to check password validity
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
-user.init(
+// Define table columns and configuration for User model
+User.init(
     {
+        // ID column
         id: {
             type: DataTypes.INTEGER,
+            allowNull: false,
             primaryKey: true,
-            autoIncrement: true,
+            autoIncrement: true
         },
+        // Username column
         username: {
             type: DataTypes.STRING,
-            allowNull: false,
+            allowNull: false
         },
+        // Email column
         email: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
-            validate: {
-                isEmail: true,
-            }
+            validate: { isEmail: true }
         },
+        // Password column
         password: {
             type: DataTypes.STRING,
             allowNull: false,
-        },
+            validate: { len: [4] } // Minimum length of 4 characters
+        }
     },
     {
+        hooks: {
+            // Hash password before User creation
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // Hash password before User update
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
+        timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'user',
+        modelName: 'user'
     }
 );
-user.hasMany(post, {
-    foreignKey: 'user_id',
-});
 
-user.hasMany(comment, {
-    foreignKey: 'user_id',
-});
-
-module.exports = user;
+module.exports = User; // Export the User model
