b3d5727dd85e23f56b0d438e13ab2c6e8a8dd444
Tova Greene <tovalgreene@gmail.com>
2023-11-13 12:09:29 -0500

    edited auth.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,7 @@
+// Middleware to check if the user is authenticated
 module.exports = (req, res, next) => {
     if (req.isAuthenticated()) {
-        return next();
+        return next(); // Proceed if authenticated
     }
-    res.redirect('/login');
+    res.redirect('/login'); // Redirect to login if not authenticated
 };
