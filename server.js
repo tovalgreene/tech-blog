25a59d52fb5caba8b9e7d3e0bde92697024ee754
Tova Greene <tovalgreene@gmail.com>
2023-11-13 12:08:51 -0500

    edited server.js

--- server.js
+++ server.js
@@ -1,33 +1,49 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-
-const sequelize = require('./config/connection');
-const routes = require('./controllers');
 const path = require('path');
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection'); // Import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-const sessionSecret = process.env.SESSION_SECRET;
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
-app.use(express.static('public'));
+// Configure session with sequelize store
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 86400000, // 1 day in milliseconds
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize }),
+};
+
+app.use(session(sess));
+
+// Set Handlebars as the view engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Set up middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-    session({
-        secret: sessionSecret,
-        resave: false,
-        saveUninitialized: true,
-    })
-);
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+// Add routing middleware
+app.use(routes);
 
+// Start the server and sync the database
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-      console.log(`App listening on port http://localhost:${PORT}`);
-    });
-  });
+  app.listen(port, () => console.log(`Now listening on port localhost:${port}`));
+});
